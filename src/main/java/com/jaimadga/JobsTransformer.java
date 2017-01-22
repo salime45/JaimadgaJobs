364fda9966764443b9a495cc726b6d905982092e
Ismael M <salime45@gmail.com>
2017-01-22 19:59:43 +0100

    - Mejorada clase transform

--- src/main/java/com/jaimadga/JobsTransformer.java
+++ src/main/java/com/jaimadga/JobsTransformer.java
@@ -3,22 +3,37 @@ package com.jaimadga;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JobsTransformer extends AbstractMessageTransformer {
 
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
 
-        String test = message.getSessionProperty("test");
+        //String test = message.getSessionProperty("test");
 
-		String msg = test + "--------------------------------------------------------------------------------";
-		
+		String msg = "";
 		
 		try {
 			msg += message.getPayloadAsString(outputEncoding);
+			JSONObject aux = new JSONObject(msg);	
+
+			String array = aux.getJSONArray("offers").toString();
+			JSONArray jobs = new JSONArray(array);
+			
+			for (int i = 0; i < jobs.length(); i++) {
+			    JSONObject row = jobs.getJSONObject(i);
+			    
+			    //System.out.println("---> " + row.toString());
+			}
+			
+			
 		} catch (Exception e) {
-			e.printStackTrace();
+			e.printStackTrace();	
 		}
+		
+
 		
 		return msg;
 	}
